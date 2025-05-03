d958ac13a013f4ed857272625da6d1761a64bfb6
AtharvaC0de <athada125@gmail.com>
2025-05-03 14:05:01 +0200

    gg

--- simple_ai.py
+++ simple_ai.py
@@ -5,16 +5,22 @@ import numpy as np
 from supremacy import helpers
 
 # This is your team name
-CREATOR = "SimSOMETHONGpleAI"
+CREATOR = "ATHARVA_BOT"
+#TankAngle=0.0
 
 
 def tank_ai(tank, info, game_map):
     """
     Function to control tanks.
     """
+#    global TankAngle
+#    tank.set_heading(TankAngle)
+#    TankAngle += 1.0
+#    if TankAngle > 360.0:
+#        TankAngle = 0.0
     if not tank.stopped:
-        if tank.stuck:
-            tank.set_heading(np.random.random() * 360.0)
+        if (tank.get_distance(tank.owner.x, tank.owner.y) > 80) or tank.stuck:
+            tank.stop()
         elif "target" in info:
             tank.goto(*info["target"])
 
@@ -24,9 +30,12 @@ def ship_ai(ship, info, game_map):
     Function to control ships.
     """
     if not ship.stopped:
+        if (ship.get_distance(ship.owner.x, ship.owner.y) > 140):
+            ship.set_heading(np.random.random() * 360.0)
         if ship.stuck:
-            if ship.get_distance(ship.owner.x, ship.owner.y) > 20:
-                ship.convert_to_base()
+            if ship.get_distance(ship.owner.x, ship.owner.y) > 40:
+                if len(info["bases"]) < 12:
+                    ship.convert_to_base()
             else:
                 ship.set_heading(np.random.random() * 360.0)
 
@@ -47,12 +56,14 @@ class PlayerAi:
     def __init__(self):
         self.team = CREATOR  # Mandatory attribute
         self.build_queue = helpers.BuildQueue(
-            ["mine", "tank", "ship", "jet"], cycle=True
+            ["mine", "tank","ship", "tank", "ship" ,"jet", "mine", "tank", "ship", "jet", "jet"], cycle=True
+        
         )
+        self.TankAngle = 0.0  # Angle for tank movement
 
     def run(self, t: float, dt: float, info: dict, game_map: np.ndarray):
         """
-        This is the main function that will be called by the game engine.
+         This is the main function that will be called by the game engine.
         """
 
         # Get information about my team
@@ -62,7 +73,16 @@ class PlayerAi:
         for base in myinfo["bases"]:
             # Calling the build_queue will return the object that was built by the base.
             # It will return None if the base did not have enough resources to build.
+            if base.mines == 3:
+                self.build_queue = helpers.BuildQueue(
+                ["mine", "ship", "tank","tank","tank","tank","tank","jet", "tank", "ship", "tank","tank","tank","tank","tank", "tank", "jet", "tank", "ship", "jet", "tank", "ship", "jet", "tank", "ship", "jet", "tank", "ship", "jet", "tank", "ship", "jet", "tank", "ship", "jet"], cycle=True)
+
             obj = self.build_queue(base)
+            if (obj is not None) and (obj.kind == 'tank'):
+                obj.set_heading(self.TankAngle)
+                self.TankAngle += 10.0
+                if self.TankAngle > 360.0:
+                    self.TankAngle = 0.0
 
         # Try to find an enemy target
         # If there are multiple teams in the info, find the first team that is not mine
